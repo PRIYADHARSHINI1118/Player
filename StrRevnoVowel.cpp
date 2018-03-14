b71024563daa3ac7a75f5e2b7d2914083af71785
Kalaiarasan.A <36396214+Kalaiarasan469@users.noreply.github.com>
2018-03-14 09:24:50 +0530

    Create StrRevnoVowel.cpp

---
+++ StrRevnoVowel.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+using namespace std;
+class StrRevnoVowel
+{
+string s;
+int n,i;
+void get()
+{
+cout<<"INPUT"<<endl;
+getline(cin,s);
+}
+void display()
+{
+cout<<"OUTPUT"<<endl;
+n=s.length();cout<<"n="<<n<<endl;
+for(i=n-1;i>=0;i--)
+{
+switch(s[i])
+{
+case 'a':break;
+case 'e':break;
+case 'i':break;
+case 'o':break;
+case 'u':break;
+default:cout<<s[i];break;
+}
+}
+}
+public:
+StrRevnoVowel()
+{
+get();
+display();
+}
+};
+int main() {
+ StrRevnoVowel s;
+	return 0;
+}
